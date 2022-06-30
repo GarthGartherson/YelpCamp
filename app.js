e5baa9c8eee9d27d6a48a93c7bcad512d5ad75c2
Garrett <garrettgreeves@gmail.com>
2022-06-30 15:11:14 -0500

    Changed login check

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
 const userRoutes = require("./routes/users");
 
-const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
+const dbUrl = process.env.DB_URL; //|| "mongodb://localhost:27017/yelp-camp";
 // process.env.DB_URL
 
 // 'mongodb://localhost:27017/yelp-camp'
